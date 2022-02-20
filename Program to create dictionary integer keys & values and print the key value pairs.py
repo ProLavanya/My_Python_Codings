804abb713539bebe23497288739144d58e8cb286
Lavanya M <99327149+ProLavanya@users.noreply.github.com>
2022-02-20 12:26:09 +0530

    Add files via upload

---
+++ Program to create dictionary integer keys & values and print the key value pairs.py
@@ -0,0 +1,9 @@
+dict_1={1:'sand',2:'sunny',3:'sunjju'}
+print(dict_1)                      # printing dictionary
+for x in dict_1:                     
+	print(x)                       #ptinting keys
+for x in dict_1.values():          #printing values
+	print(x)               
+for i,j in dict_1.items():      #printing keys and value pairs
+	print(i,":",j)	
+
