a0f2fdad17ed7c837154a739642345d6f1105a31
Lavanya M <99327149+ProLavanya@users.noreply.github.com>
2022-02-20 12:25:01 +0530

    Add files via upload

---
+++ print keys and values in dictionary with special pattern .py
@@ -0,0 +1,8 @@
+d={}
+d[1]="python"  # Which is the keys equal to values 
+d[2]='java'
+d[3]='c'
+d[4]='c++' 
+d[5]="Data science"
+for i,j in d.items(): # here dictionary consisting keys and values
+	print(i,"--->",j)
